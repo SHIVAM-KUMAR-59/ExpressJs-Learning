1ee7d8809d83f34f4c04fb7a2f9c130bb3930ad5
SHIVAM-KUMAR-59 <jsrshivamx7@gmail.com>
2024-09-01 13:15:18 +0530

    01-09-2024

--- src/index.mjs
+++ src/index.mjs
@@ -4,6 +4,10 @@ import express from "express";
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.get("/", (req, res) => {
+  res.status(200).send("Hello World!");
+});
+
 // Creating a new express app instance
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
