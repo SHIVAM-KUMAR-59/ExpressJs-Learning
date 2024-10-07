7c6c6b34d755ea1152299fc08c2532bcb09538b9
SHIVAM-KUMAR-59 <jsrshivamx7@gmail.com>
2024-10-07 11:36:15 +0530

    Cookies-Parser Added

--- src/index.mjs
+++ src/index.mjs
@@ -1,15 +1,18 @@
 import express from "express"; // Importing express
 import routes from "./routes/index.mjs";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
 app.use(express.json()); // Middleware to parse JSON body
+app.use(cookieParser("HelloWorld")); // Parsing cookies
 app.use(routes); // Using all the routes
 
 const PORT = process.env.PORT || 3000; // Declaring the port
 
 // Get Requests for home
 app.get("/", (req, res) => {
+  res.cookie("Hello", "World", { maxAge: 30000, signed: true });
   res.status(200).send({ msg: "Hello World" });
 });
 
--- src/routes/products.mjs
+++ src/routes/products.mjs
@@ -5,7 +5,14 @@ const router = Router();
 
 // Get Requests for /api/products route (Shows All Products)
 router.get("/api/products", (req, res) => {
-  res.status(200).send(products);
+  console.log(req.headers.cookie);
+  console.log(req.cookies);
+  console.log(req.signedCookies.Hello);
+  if (req.signedCookies.Hello && req.signedCookies.Hello === "WORLD") {
+    return res.status(200).send(products);
+  } else {
+    return res.status(403).send({ msg: "Sorry incorrect cookie" });
+  }
 });
 
 export default router;
