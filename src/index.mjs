f32b14a04ad7bf3f147977933522a806f27f224b
SHIVAM-KUMAR-59 <jsrshivamx7@gmail.com>
2024-10-07 12:30:38 +0530

    Basic Validation Added

--- src/index.mjs
+++ src/index.mjs
@@ -2,6 +2,7 @@ import express from "express"; // Importing express
 import routes from "./routes/index.mjs";
 import cookieParser from "cookie-parser";
 import session from "express-session";
+import { users } from "./utils/constants.mjs";
 
 const app = express();
 
@@ -28,6 +29,72 @@ app.get("/", (req, res) => {
   req.session.visited = true;
   res.cookie("Hello", "World", { maxAge: 30000, signed: true });
   res.status(200).send({ msg: "Hello World" });
+});
+
+// Path to authenticate user
+app.post("/api/auth", (req, res) => {
+  // Assuming the payload is correct
+  const {
+    body: { name, password },
+  } = req;
+
+  const findUser = users.find((user) => user.name === name);
+
+  // User Not Found
+  if (!findUser) {
+    return res.status(401).send({ msg: "User Not Found" });
+  }
+
+  // Incorrect Password
+  if (findUser.password !== password) {
+    return res.status(401).send({ msg: "Incorrect Password" });
+  }
+
+  req.session.user = findUser; // Mapping the user to the session
+  return res.status(200).send(findUser);
+});
+
+// Get the authentication status of the user
+app.get("/api/auth/status", (req, res) => {
+  req.sessionStore.get(req.sessionId, (err, session) => {
+    if (err) {
+      console.log(err);
+      throw err;
+    } else {
+      console.log(session);
+    }
+  });
+  return req.session.user
+    ? res.status(200).send(req.session.user)
+    : res.status(401).send({ msg: "Not Authenticated" });
+});
+
+// Route to add item to the cart
+app.post("/api/cart", (req, res) => {
+  if (!req.session.user) {
+    return res.status(401).send({ msg: "Unauthorized" });
+  }
+
+  const { body: item } = req;
+  const { cart } = req.session;
+  if (cart) {
+    // Cart is defined, push the item into it
+    cart.push(item);
+  } else {
+    // Cart is not defined, then define it
+    req.session.cart = [item];
+  }
+
+  return res.status(201).send(item);
+});
+
+// Get items from the cart
+app.get("/api/cart", (req, res) => {
+  if (!req.session.user) {
+    return res.status(401).send({ msg: "Unauthorized" });
+  }
+
+  return res.send(req.session.cart ?? []);
 });
 
 // Listening on the server
--- src/utils/constants.mjs
+++ src/utils/constants.mjs
@@ -1,11 +1,11 @@
 // Users array
 export const users = [
-  { id: 1, name: "Shivam", email: "shivam@gmail.com" },
-  { id: 2, name: "Rohan", email: "rohan@gmail.com" },
-  { id: 3, name: "Hardik", email: "hardik@gmail.com" },
-  { id: 4, name: "Pihu", email: "pihu@gmail.com" },
-  { id: 5, name: "Ritu", email: "rituu@gmail.com" },
-  { id: 6, name: "Lihu", email: "lihu@gmail.com" },
+  { id: 1, name: "Shivam", email: "shivam@gmail.com", password: "shivam123" },
+  { id: 2, name: "Rohan", email: "rohan@gmail.com", password: "rohan123" },
+  { id: 3, name: "Hardik", email: "hardik@gmail.com", password: "hardik123" },
+  { id: 4, name: "Pihu", email: "pihu@gmail.com", password: "pihu123" },
+  { id: 5, name: "Ritu", email: "rituu@gmail.com", password: "ritu123" },
+  { id: 6, name: "Lihu", email: "lihu@gmail.com", password: "lihu123" },
 ];
 
 // Products array
